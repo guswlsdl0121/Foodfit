90bffe83db3db64e4a57d63a30d1fcffe945d94f
hyunjin <guswlsdl0121@gmail.com>
2023-06-27 03:29:39 +0900

    update add UserFood

--- BackEnd/src/main/java/Foodfit/BackEnd/Controller/UserFoodController.java
+++ BackEnd/src/main/java/Foodfit/BackEnd/Controller/UserFoodController.java
@@ -8,10 +8,9 @@ import Foodfit.BackEnd.Service.UserFoodService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user-foods")
@@ -20,9 +19,9 @@ public class UserFoodController {
     private final UserFoodService userFoodService;
 
     @PostMapping
-    public ResponseEntity<UserFood> addUserFood(@RequestBody UserFoodDTO userFoodDTO) {
-        UserFood userFood = userFoodService.addUserFood(userFoodDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userFood);
+    public ResponseEntity<List<UserFood>> addUserFood(@RequestBody UserFoodDTO userFoodDTO) {
+        List<UserFood> userFoods = userFoodService.addUserFoods(userFoodDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userFoods);
     }
 }
 
