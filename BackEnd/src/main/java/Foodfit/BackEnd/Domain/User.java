9ea068d12132112a600d56cbc5d443531c9b3910
hyunjin <guswlsdl0121@gmail.com>
2023-06-27 00:50:49 +0900

    Add P6spy and Simple Test

--- BackEnd/src/main/java/Foodfit/BackEnd/Domain/User.java
+++ BackEnd/src/main/java/Foodfit/BackEnd/Domain/User.java
@@ -1,9 +1,13 @@
 package Foodfit.BackEnd.Domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Setter
+@Getter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
