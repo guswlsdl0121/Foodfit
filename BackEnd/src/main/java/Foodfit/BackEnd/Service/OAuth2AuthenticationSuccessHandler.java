242881b138d4bb35484d79c8d456274cd1e3e6cd
minturtle <minseok0425@kakao.com>
2023-06-27 13:07:34 +0900

    add JWT library

---
+++ BackEnd/src/main/java/Foodfit/BackEnd/Service/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,26 @@
+package Foodfit.BackEnd.Service;
+
+import Foodfit.BackEnd.repository.UserRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+// 로그인 성공 후 부가 처리를 하는 클래스
+@RequiredArgsConstructor
+@Component
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+}
