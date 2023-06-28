ef3cddbccf799b6cfa1f2a2c2b6d16e963f0f6d9
minturtle <minseok0425@kakao.com>
2023-06-28 11:07:53 +0900

    add User Field Verifier

--- BackEnd/src/main/java/Foodfit/BackEnd/Utils/UserProvider.java
+++ BackEnd/src/main/java/Foodfit/BackEnd/Utils/UserProvider.java
@@ -2,11 +2,14 @@ package Foodfit.BackEnd.Utils;
 
 import Foodfit.BackEnd.Domain.User;
 import Foodfit.BackEnd.Repository.UserRepository;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.util.Reflection;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
 import java.util.Optional;
 
 @Component
@@ -35,9 +38,41 @@ public class UserProvider {
         if (principal instanceof Long) {
             Long userId = (Long) principal;
             return userRepository.findById(userId);
-        } else {
-            // 사용자 ID를 가져올 수 없는 경우 예외 처리 또는 다른 처리를 수행할 수 있습니다.
-            return Optional.empty();
         }
+
+
+        return Optional.empty();
+
+    }
+
+
+    /*
+     * methodName : verifyIsFieldNotNull
+     * author : minturtle
+     * description : 필드 이름을 받아, 전달 받은 모든 필드가 Not NULL인지 확인하는 메서드
+     * @param userEntity(User), fieldNames(String ...)
+     * @return 모두 not null이라면 true, 하나라도 null인 필드가 있다면 false
+     */
+    public boolean verifyIsFieldNotNull(User user, UserFields ... fieldNames) throws NoSuchFieldException, IllegalAccessException {
+        // Java Reflection을 사용해 테스트 진행
+        Class<? extends User> userClass = user.getClass();
+
+        for(UserFields fieldName : fieldNames){
+            Field field = userClass.getField(fieldName.name());
+
+            boolean preAccess = field.canAccess(user); //접근 제한을 다시 원래대로 돌리기 위해 백업
+            field.setAccessible(true);
+
+            Object actual = field.get(user); // 해당 필드의 user의 값을 받아옴.
+            field.setAccessible(preAccess);
+
+            if(actual == null) return false;
+        }
+        return true;
+    }
+
+
+    public  enum UserFields{
+        id, name, age, gender, uid;
     }
 }
