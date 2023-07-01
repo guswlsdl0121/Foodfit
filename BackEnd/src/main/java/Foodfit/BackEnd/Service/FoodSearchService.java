763c69c21bfbfa12870308f3e7cc0a6d997213b3
hyunjin <guswlsdl0121@gmail.com>
2023-07-01 20:57:56 +0900

    검색 기능 로직 개선

--- BackEnd/src/main/java/Foodfit/BackEnd/Service/FoodSearchService.java
+++ BackEnd/src/main/java/Foodfit/BackEnd/Service/FoodSearchService.java
@@ -4,14 +4,12 @@ import Foodfit.BackEnd.Domain.Food;
 import Foodfit.BackEnd.Repository.FoodRepository;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static Foodfit.BackEnd.Exception.FoodException.*;
 
 @Service
 @RequiredArgsConstructor
@@ -27,8 +25,33 @@ public class FoodSearchService {
      * @return foodPage.getContent();
      */
     public List<Food> searchFoods(String name) {
-        Pageable pageable = PageRequest.of(0, 10, Sort.by("name").ascending());
-        Page<Food> foodPage = foodRepository.findByNameContainingIgnoreCase(name, pageable);
-        return foodPage.getContent();
+        List<Food> foods = foodRepository.findTop10ByNameContainingIgnoreCase(name);
+        // 영어로 검색된 경우 처리
+        if (containsEnglish(name)) {
+            throw new EnglishSearchException("영어로 검색된 결과입니다.");
+        }
+
+        if (foods.isEmpty())  {
+            throw new NoSearchResultException("검색 조건에 맞는 음식이 없습니다.");
+        }
+
+        return foods;
     }
+
+    /**
+     * methodName : containsEnglish
+     * author : guswlsdl
+     * description : 검색어에 영어가 포함되어있는지 확인한다.
+     * @param  name
+     * @return boolean
+     */
+    private boolean containsEnglish(String name) {
+        for (char c : name.toCharArray()) {
+            if (Character.isLetter(c) && Character.UnicodeBlock.of(c) == Character.UnicodeBlock.BASIC_LATIN) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
