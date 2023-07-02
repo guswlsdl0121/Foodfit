1290c076cb5d0355bcb691a211fe602e4ed656a9
hyunjin <guswlsdl0121@gmail.com>
2023-07-03 05:50:48 +0900

    검색 기능 구조 개선

--- BackEnd/src/main/java/Foodfit/BackEnd/Repository/FoodRepository.java
+++ BackEnd/src/main/java/Foodfit/BackEnd/Repository/FoodRepository.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
     List<Food> findTop10ByNameContainingIgnoreCase(String name);
-    //TODO [HJ] 성능 비교를 위한 페이지네이션  메서드
-    Page<Food> findAllByNameContainingIgnoreCase(String name, Pageable pageable);
     List<Food> findAllByIdIn(List<Long> ids);
 }
 
--- BackEnd/src/main/java/Foodfit/BackEnd/Service/FoodSearchService.java
+++ BackEnd/src/main/java/Foodfit/BackEnd/Service/FoodSearchService.java
@@ -14,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static Foodfit.BackEnd.Exception.BadRequestException.*;
 
 @Service
 @RequiredArgsConstructor
@@ -27,44 +26,16 @@ public class FoodSearchService {
      * author : guswlsdl
      * description : 검색어에 대한 상위 10개 항목의 검색 결과를 가져온다.
      * @param  name
-     * @return foodPage.getContent();
+     * @return foods;
      */
     @TimeLog
     public List<Food> searchFoods(String name) {
         List<Food> foods = foodRepository.findTop10ByNameContainingIgnoreCase(name);
 
         //예외 처리
-        if (containsEnglish(name)) throw new EnglishSearchException();
         if (foods.isEmpty()) throw new NoFoodException();
 
         return foods;
-    }
-
-    //TODO [HJ] 성능 비교를 위핸 Pagination을 이용한 코드
-    public List<Food> searchFoodsWithPagination(String name, int page, int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        Page<Food> foodPage = foodRepository.findAllByNameContainingIgnoreCase(name, pageable);
-
-        if (containsEnglish(name)) throw new EnglishSearchException();
-        if (foodPage.isEmpty()) throw new NoFoodException();
-
-        return foodPage.getContent();
-    }
-
-    /**
-     * methodName : containsEnglish
-     * author : guswlsdl
-     * description : 검색어에 영어가 포함되어있는지 확인한다.
-     * @param  name
-     * @return boolean
-     */
-    private boolean containsEnglish(String name) {
-        for (char c : name.toCharArray()) {
-            if (Character.isLetter(c) && Character.UnicodeBlock.of(c) == Character.UnicodeBlock.BASIC_LATIN) {
-                return true;
-            }
-        }
-        return false;
     }
 
 }
